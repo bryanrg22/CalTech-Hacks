a8b04fafe8ddc691703bfbb12a867779c827f4b0
Jake Grigorian <66845894+jgrigorian23@users.noreply.github.com>
2025-04-25 23:27:22 -0700

    firebase setup

---
+++ temp.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCel2lcphKfP3ruLV5v-P9aRtgVhnjI7uI",
+  authDomain: "hacktech-cce3f.firebaseapp.com",
+  projectId: "hacktech-cce3f",
+  storageBucket: "hacktech-cce3f.firebasestorage.app",
+  messagingSenderId: "486964013965",
+  appId: "1:486964013965:web:57a52ce5fcc5de9e47ebc4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
